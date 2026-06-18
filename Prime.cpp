fd7b0d6e094735728720e2ace481fd4cdcfd345d
shindechaitanya013 <shindechaitanya013@gmail.com>
2026-06-18 22:17:51 +0530

    Add Program to check prime number

---
+++ Prime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    int cnt = 0;
+    for(int i=1; i<n; i++){
+        if(n%i == 0){
+            cnt++;
+        }
+    }
+
+    if(cnt == 2)
+        cout<<n <<" is a prime Number.";
+    else
+        cout<<n <<" is not a prime number.";
+
+
+    return 0;
+}
