3b9f07110a6a666aab7231685e58a8676a336121
shindechaitanya013 <shindechaitanya013@gmail.com>
2026-06-21 22:31:50 +0530

    Added Program Find All Pairs with Given Sum in an Array

--- Arrays/AllPairSum.cpp
+++ Arrays/AllPairSum.cpp
@@ -1,4 +1,5 @@
-// Find a Pair with Given Sum in an Array
+// Find All Pairs with Given Sum in an Array
+
 #include<iostream>
 using namespace std;
 
@@ -10,7 +11,7 @@ int main(){
 
     int arr[n];
 
-    cout<<"Enter Array elements: ";
+    cout<<"Enter array elements: ";
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
@@ -18,17 +19,19 @@ int main(){
     cout<<"Enter Sum: ";
     cin>>sum;
 
+    bool found = false;
     for(int i=0; i<n; i++){
         for(int j=i+1; j<n; j++){
             if(arr[i] + arr[j] == sum){
-                cout<<"Pair found: "<<arr[i]<<" "<<arr[j];
-                return 0;
+                cout<<arr[i] <<" "<<arr[j] <<endl;
+                found = true;
             }
         }
     }
 
-    cout<<"No pair found";
-
+    if(found == false){
+        cout<<"Not Found.";
+    }
 
     return 0;
 }
